762bb6b206fe213537e1db8aeb7ae1457e1c4923
DaveWill26 <155826534+DaveWill26@users.noreply.github.com>
2024-12-10 01:22:16 +0800

    try nyo toh

---
+++ POSSystem/Inventory.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace POSSystem
+{
+    partial class Inventory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Inventory));
+            this.btnPOS = new System.Windows.Forms.Button();
+            this.btnIMS = new System.Windows.Forms.Button();
+            this.btnCUS = new System.Windows.Forms.Button();
+            this.btnREP = new System.Windows.Forms.Button();
+            this.btnADM = new System.Windows.Forms.Button();
+            this.btnSET = new System.Windows.Forms.Button();
+            this.panelList = new System.Windows.Forms.Panel();
+            this.btnMain = new System.Windows.Forms.Button();
+            this.panelList2 = new System.Windows.Forms.Panel();
+            this.btnAddOns = new System.Windows.Forms.Button();
+            this.btnUtilities = new System.Windows.Forms.Button();
+            this.panelList3 = new System.Windows.Forms.Panel();
+            this.button18 = new System.Windows.Forms.Button();
+            this.panelList.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnPOS
+            // 
+            this.btnPOS.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnPOS.BackgroundImage")));
+            this.btnPOS.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnPOS.Location = new System.Drawing.Point(0, 85);
+            this.btnPOS.Name = "btnPOS";
+            this.btnPOS.Size = new System.Drawing.Size(100, 129);
+            this.btnPOS.TabIndex = 53;
+            this.btnPOS.UseVisualStyleBackColor = true;
+            this.btnPOS.Click += new System.EventHandler(this.btnPOS_Click);
+            // 
+            // btnIMS
+            // 
+            this.btnIMS.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnIMS.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnIMS.BackgroundImage")));
+            this.btnIMS.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnIMS.Location = new System.Drawing.Point(0, 213);
+            this.btnIMS.Name = "btnIMS";
+            this.btnIMS.Size = new System.Drawing.Size(100, 130);
+            this.btnIMS.TabIndex = 54;
+            this.btnIMS.UseVisualStyleBackColor = true;
+            // 
+            // btnCUS
+            // 
+            this.btnCUS.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnCUS.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnCUS.BackgroundImage")));
+            this.btnCUS.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnCUS.Location = new System.Drawing.Point(0, 341);
+            this.btnCUS.Name = "btnCUS";
+            this.btnCUS.Size = new System.Drawing.Size(100, 130);
+            this.btnCUS.TabIndex = 55;
+            this.btnCUS.UseVisualStyleBackColor = true;
+            // 
+            // btnREP
+            // 
+            this.btnREP.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnREP.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnREP.BackgroundImage")));
+            this.btnREP.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnREP.Location = new System.Drawing.Point(0, 469);
+            this.btnREP.Name = "btnREP";
+            this.btnREP.Size = new System.Drawing.Size(100, 130);
+            this.btnREP.TabIndex = 56;
+            this.btnREP.UseVisualStyleBackColor = true;
+            // 
+            // btnADM
+            // 
+            this.btnADM.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnADM.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnADM.BackgroundImage")));
+            this.btnADM.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnADM.Location = new System.Drawing.Point(0, 597);
+            this.btnADM.Name = "btnADM";
+            this.btnADM.Size = new System.Drawing.Size(100, 130);
+            this.btnADM.TabIndex = 57;
+            this.btnADM.UseVisualStyleBackColor = true;
+            this.btnADM.Click += new System.EventHandler(this.btnADM_Click);
+            // 
+            // btnSET
+            // 
+            this.btnSET.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnSET.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnSET.BackgroundImage")));
+            this.btnSET.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnSET.Location = new System.Drawing.Point(0, 725);
+            this.btnSET.Name = "btnSET";
+            this.btnSET.Size = new System.Drawing.Size(100, 126);
+            this.btnSET.TabIndex = 58;
+            this.btnSET.UseVisualStyleBackColor = true;
+            // 
+            // panelList
+            // 
+            this.panelList.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panelList.Controls.Add(this.panelList3);
+            this.panelList.Controls.Add(this.panelList2);
+            this.panelList.Location = new System.Drawing.Point(134, 255);
+            this.panelList.Name = "panelList";
+            this.panelList.Size = new System.Drawing.Size(1167, 572);
+            this.panelList.TabIndex = 59;
+            // 
+            // btnMain
+            // 
+            this.btnMain.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMain.Location = new System.Drawing.Point(489, 170);
+            this.btnMain.Name = "btnMain";
+            this.btnMain.Size = new System.Drawing.Size(147, 41);
+            this.btnMain.TabIndex = 60;
+            this.btnMain.Text = "MAIN";
+            this.btnMain.UseVisualStyleBackColor = true;
+            this.btnMain.Click += new System.EventHandler(this.btnMain_Click);
+            // 
+            // panelList2
+            // 
+            this.panelList2.Location = new System.Drawing.Point(3, 0);
+            this.panelList2.Name = "panelList2";
+            this.panelList2.Size = new System.Drawing.Size(1161, 572);
+            this.panelList2.TabIndex = 0;
+            // 
+            // btnAddOns
+            // 
+            this.btnAddOns.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddOns.Location = new System.Drawing.Point(645, 171);
+            this.btnAddOns.Name = "btnAddOns";
+            this.btnAddOns.Size = new System.Drawing.Size(146, 39);
+            this.btnAddOns.TabIndex = 61;
+            this.btnAddOns.Text = "ADD ONS";
+            this.btnAddOns.UseVisualStyleBackColor = true;
+            this.btnAddOns.Click += new System.EventHandler(this.btnAddOns_Click);
+            // 
+            // btnUtilities
+            // 
+            this.btnUtilities.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUtilities.Location = new System.Drawing.Point(801, 170);
+            this.btnUtilities.Name = "btnUtilities";
+            this.btnUtilities.Size = new System.Drawing.Size(147, 39);
+            this.btnUtilities.TabIndex = 62;
+            this.btnUtilities.Text = "UTILITIES";
+            this.btnUtilities.UseVisualStyleBackColor = true;
+            this.btnUtilities.Click += new System.EventHandler(this.btnUtilities_Click);
+            // 
+            // panelList3
+            // 
+            this.panelList3.Location = new System.Drawing.Point(0, 0);
+            this.panelList3.Name = "panelList3";
+            this.panelList3.Size = new System.Drawing.Size(1164, 569);
+            this.panelList3.TabIndex = 0;
+            // 
+            // button18
+            // 
+            this.button18.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.button18.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button18.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button18.ForeColor = System.Drawing.Color.White;
+            this.button18.Location = new System.Drawing.Point(80, 40);
+            this.button18.Name = "button18";
+            this.button18.Size = new System.Drawing.Size(104, 28);
+            this.button18.TabIndex = 63;
+            this.button18.Text = "Logout";
+            this.button18.UseVisualStyleBackColor = false;
+            this.button18.Click += new System.EventHandler(this.button18_Click);
+            // 
+            // Inventory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1336, 881);
+            this.Controls.Add(this.button18);
+            this.Controls.Add(this.btnUtilities);
+            this.Controls.Add(this.btnAddOns);
+            this.Controls.Add(this.btnMain);
+            this.Controls.Add(this.panelList);
+            this.Controls.Add(this.btnSET);
+            this.Controls.Add(this.btnADM);
+            this.Controls.Add(this.btnREP);
+            this.Controls.Add(this.btnCUS);
+            this.Controls.Add(this.btnIMS);
+            this.Controls.Add(this.btnPOS);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Inventory";
+            this.Text = "Inventory";
+            this.panelList.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPOS;
+        private System.Windows.Forms.Button btnIMS;
+        private System.Windows.Forms.Button btnCUS;
+        private System.Windows.Forms.Button btnREP;
+        private System.Windows.Forms.Button btnADM;
+        private System.Windows.Forms.Button btnSET;
+        private System.Windows.Forms.Panel panelList;
+        private System.Windows.Forms.Button btnMain;
+        private System.Windows.Forms.Panel panelList2;
+        private System.Windows.Forms.Button btnAddOns;
+        private System.Windows.Forms.Button btnUtilities;
+        private System.Windows.Forms.Panel panelList3;
+        private System.Windows.Forms.Button button18;
+    }
+}
---
+++ POSSystem/Inventory.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POSSystem
+{
+    public partial class Inventory : Form
+    {
+        public Inventory()
+        {
+            InitializeComponent();
+        }
+        private void ShowPanel(Panel panelToShow)
+        {
+
+            // Hide all panels
+            panelList.Visible = false;
+            panelList2.Visible = false;
+            panelList3.Visible = false;
+
+            // Show the desired panel
+            panelToShow.Visible = true;
+
+        }
+
+        private void btnMain_Click(object sender, EventArgs e)
+        {
+            string[,] data = {
+                { "Regular Coffee", "123456", "15/45", "Jan 2025", "Starbucks" },
+                { "Espresso Shot", "789012", "20/60", "Dec 2025", "Lavazza" },
+                { "Decaf Coffee", "345678", "10/30", "Feb 2025", "Peet's Coffee" },
+                { "Colombian Coffee", "901234", "25/75", "Mar 2025", "Dunkin Donuts" },
+                { "French Vanilla Syrup", "567890", "20/60", "May 2025", "Torani" },
+                { "Caramel Syrup", "246801", "20/60", "Jun 2025", "Monin" },
+                { "Hazelnut Syrup", "135792", "15/45", "Jul 2025", "DaVinci" },
+                { "White Mocha Syrup", "314159", "25/75", "Sep 2025", "Ghirardelli" },
+                { "Chocolate Syrup", "271828", "20/60", "Oct 2025", "Hershey's" },
+                { "Cinnamon Dolce Syrup", "987654", "15/45", "Nov 2025", "Starbucks" },
+                { "Peppermint Syrup", "246810", "15/45", "Nov 2025", "Monin" },
+                { "Vanilla Powder", "111222", "20/60", "Jan 2025", "Big Train" },
+                { "Cocoa Powder", "333444", "15/45", "Feb 2025", "Guitard" },
+                { "Chai Tea Powder", "555666", "25/75", "Mar 2025", "Oregon Chai" },
+                { "Matcha Green Tea Powder", "777888", "20/60", "Apr 2025", "Ito En" },
+                { "Earl Grey Tea", "999000", "20/60", "Jun 2025", "Twinings" },
+                { "English Breakfast Tea", "111333", "20/60", "Jun 2025", "Taylors of Harrogate" },
+                { "Hot Cocoa Mix", "333555", "20/60", "Aug 2025", "Swiss Miss" },
+                { "Swiss Miss", "444777", "20/60", "Sep 2025", "Hills Bros." },
+                { "White Hot Chocolate Mix", "666999", "20/60", "Nov 2025", "Stephen's Gourmet" },
+                { "Chocolate Muffin", "888111", "20/60", "Nov 2025", "Otis Spunkmeyer" },
+                { "Blueberry Muffin", "222333", "20/60", "Dec 2025", "Sara Lee" },
+                { "Chocolate Croissant", "444555", "25/75", "Jan 2026", "Pepperidge Farm" }
+            };
+            DataTable table = new DataTable();
+
+            for (int col = 0; col < data.GetLength(1); col++)
+            {
+                table.Columns.Add();
+            }
+            for (int i = 0; i < data.GetLength(0); i++)
+            {
+                table.Rows.Add(data[i, 0], data[i, 1], data[i, 2], data[i, 3], data[i, 4]);
+            }
+            DataGridView dataGridView = new DataGridView
+            {
+                DataSource = table,
+                Dock = DockStyle.Fill,
+                AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill,
+                AllowUserToAddRows = false
+            };
+            dataGridView.ColumnHeadersVisible = false;
+            panelList.Controls.Clear();
+            panelList.Controls.Add(dataGridView);
+
+        }
+
+        private void btnAddOns_Click(object sender, EventArgs e)
+        {
+            string[,] data1 = {
+        { "Boba pearl", "123456", "15/45", "Jan 2025", "Starbucks" },
+        { "Grass jelly", "789012", "20/60", "Dec 2025", "Lavazza" },
+        { "Aloe vera", "345678", "10/30", "Feb 2025", "Peet's Coffee" },
+        { "Pudding", "901234", "25/75", "Mar 2025", "Dunkin Donuts" },
+        { "Fruit jelly", "567890", "20/60", "May 2025", "Torani" },
+        { "Red bean", "246801", "20/60", "Jun 2025", "Monin" },
+        { "Tapioca balls", "135792", "15/45", "Jul 2025", "DaVinci" },
+        { "White mocha syrup", "314159", "25/75", "Sep 2025", "Ghirardelli" },
+        { "Coconut jelly", "271828", "20/60", "Oct 2025", "Hershey's" },
+        { "Mochi", "987654", "15/45", "Nov 2025", "Starbucks" },
+        { "Peppermint syrup", "246810", "15/45", "Nov 2025", "Monin" },
+        { "Egg pudding", "111222", "20/60", "Jan 2025", "Big Train" },
+        { "Rainbow jelly", "333444", "35/105", "Feb 2025", "Guittard" },
+        { "Chai Tea Powder", "555666", "25/75", "Mar 2025", "Oregon Chai" },
+        { "Lychee jelly", "777888", "20/60", "Apr 2025", "Ito En" },
+        { "Mango jelly", "999000", "20/60", "Jun 2025", "Twinings" },
+        { "Green apple", "111333", "20/60", "Jun 2025", "Taylors of Harrogate" },
+        { "Chocolate", "333555", "20/60", "Aug 2025", "Swiss Miss" },
+        { "Caramel", "444777", "20/60", "Sep 2025", "Hills Bros." },
+        { "Almond", "666999", "20/60", "Nov 2025", "Stephen's Gourmet" },
+        { "Hazelnut", "888111", "20/60", "Nov 2025", "Otis Spunkmeyer" },
+        { "Oreo", "222333", "20/60", "Dec 2025", "Sara Lee" },
+        { "Honey", "444555", "25/75", "Jan 2026", "Pepperidge Farm" }
+      };
+
+            DataTable innerTable = new DataTable();
+            for (int col = 0; col < data1.GetLength(1); col++)
+            {
+                innerTable.Columns.Add(new DataColumn());
+            }
+            for (int i = 0; i < data1.GetLength(0); i++)
+            {
+                innerTable.Rows.Add(data1[i, 0], data1[i, 1], data1[i, 2], data1[i, 3], data1[i, 4]);
+            }
+
+            DataGridView dataGridView1 = new DataGridView
+            {
+                DataSource = innerTable,
+                Dock = DockStyle.Fill,
+                AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill,
+                AllowUserToAddRows = false
+            };
+
+            dataGridView1.ColumnHeadersVisible = false;
+            panelList2.Controls.Clear();
+            panelList2.Controls.Add(dataGridView1);
+        }
+
+        private void btnUtilities_Click(object sender, EventArgs e)
+        {
+            string[,] data2 = {
+        { "Sealer machine", "123456", "15/45", "Jan 2025", "Starbucks" },
+        { "Blender", "789012", "20/60", "Dec 2025", "Lavazza" },
+        { "Spoons", "345678", "10/30", "Feb 2025", "Peet's Coffee" },
+        { "Disposable cups", "901234", "25/75", "Mar 2025", "Dunkin Donuts" },
+        { "Menu board", "567890", "20/60", "May 2025", "Torani" },
+        { "White mocha syrup", "246801", "20/60", "Jun 2025", "Monin" },
+        { "Cleaning supplies", "135792", "15/45", "Jul 2025", "DaVinci" },
+        { "Mochi", "314159", "25/75", "Sep 2025", "Ghirardelli" },
+        { "Trash can", "271828", "20/60", "Oct 2025", "Hershey's" },
+        { "Recycling bins", "987654", "15/45", "Nov 2025", "Starbucks" },
+        { "Storage shelves", "246810", "15/45", "Nov 2025", "Monin" },
+        { "Chai tea powder", "111222", "20/60", "Jan 2025", "Big Train" },
+        { "Delivery bags", "333444", "15/45", "Feb 2025", "Guitard" },
+        { "Mango jelly", "555666", "25/75", "Mar 2025", "Oregon Chai" },
+        { "Display cases", "777888", "20/60", "Apr 2025", "Ito En" },
+        { "Ice machine", "999000", "20/60", "Jun 2025", "Twinings" },
+        { "Caramel", "111333", "20/60", "Jun 2025", "Taylors of Harrogate" },
+        { "Tea brewing equipment", "333555", "20/60", "Aug 2025", "Swiss Miss" },
+        { "Seating", "444777", "20/60", "Sep 2025", "Hills Bros." },
+        { "Water filtration system", "666999", "20/60", "Nov 2025", "Stephen's Gourmet" },
+        { "Music system", "888111", "20/60", "Nov 2025", "Otis Spunkmeyer" },
+        { "Straws", "222333", "20/60", "Dec 2025", "Sara Lee" },
+        { "Spoons", "444555", "25/75", "Jan 2026", "Pepperidge Farm" }
+    };
+
+            DataTable innerTable1 = new DataTable();
+            for (int col = 0; col < data2.GetLength(1); col++)
+            {
+                innerTable1.Columns.Add(new DataColumn());
+            }
+            for (int i = 0; i < data2.GetLength(0); i++)
+            {
+                innerTable1.Rows.Add(data2[i, 0], data2[i, 1], data2[i, 2], data2[i, 3], data2[i, 4]);
+            }
+
+            DataGridView dataGridView2 = new DataGridView
+            {
+                DataSource = innerTable1,
+                Dock = DockStyle.Fill,
+                AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill,
+                AllowUserToAddRows = false
+            };
+
+            dataGridView2.ColumnHeadersVisible = false;
+            panelList3.Controls.Clear();
+            panelList3.Controls.Add(dataGridView2);
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnPOS_Click(object sender, EventArgs e)
+        {
+            POS pos1 = new POS();
+            pos1.Show();
+            this.Hide();
+        }
+
+        private void btnADM_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
